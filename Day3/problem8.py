5a503691468a526e8a4872ab4b1659412198f51f
potes <s.potes@utp.edu.co>
2023-12-06 06:39:05 -0500

    Day3

---
+++ Day3/problem8.py
@@ -0,0 +1,45 @@
+"""
+Question 8
+Level 2
+
+Question:
+Write a program that accepts a comma separated sequence 
+of words as input and prints the words in a comma-separated 
+sequence after sorting them alphabetically.
+Suppose the following input is supplied to the program:
+without,hello,bag,world
+Then, the output should be:
+bag,hello,without,world
+
+Hints:
+In case of input data being supplied to the question,
+it should be assumed to be a console input.
+"""
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+words = input("enter words in a comma separated sequence --> ").split(",")
+sorted_list = merge_sort(words)
+print("Sorted List:", sorted_list)
