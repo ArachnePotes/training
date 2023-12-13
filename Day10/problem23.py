e5d5ba3b062bb5b5c4e495f3152b06139e00afb6
potes <s.potes@utp.edu.co>
2023-12-13 06:34:53 -0500

    Da

---
+++ Day10/problem23.py
@@ -0,0 +1,18 @@
+'''
+Question 23
+level 1
+
+Question:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+    '''
+
+def square(x):
+    return x**2
+
+n = [1,4,5,6,8,2,3]
+
+for i in n:
+    print(square(i))
