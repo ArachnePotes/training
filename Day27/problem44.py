95907f6f9693ff2064a2746733ef98bc9f1f510b
potes <s.potes@utp.edu.co>
2023-12-30 11:16:49 -0500

    Day27

---
+++ Day27/problem44.py
@@ -0,0 +1,16 @@
+'''
+
+Question44:
+Write a program which can filter() to make a list whose elements are even number between 1 and 20 
+(both included).
+
+Hints:
+
+Use filter() to filter elements of a list.
+Use lambda to define anonymous functions.
+
+
+'''
+
+Evenlist = list(filter(lambda x:x%2==0,range(1,21)))
+print(Evenlist)
