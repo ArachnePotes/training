cb468962de09ce012196a19c7173aa62aa6110f3
potes <s.potes@utp.edu.co>
2024-01-19 08:02:16 -0500

    Day46

---
+++ Day40/Problem57.py
@@ -0,0 +1,13 @@
+'''
+Question:
+
+
+Print a unicode string "hello world".
+
+Hints:
+
+Use u'strings' format to define unicode string.
+'''
+
+unicodeStr  = u'hello world'
+print(f"{unicodeStr} Type: {type(unicodeStr)}")
